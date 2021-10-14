69b32ae87fb9a7c8336df172433492b4482b00e8
Samsonov Dima <sdima1357@gmail.com>
2021-10-14 12:16:03 +0300

    Update universe.c

--- src/universe.c
+++ src/universe.c
@@ -1,6 +1,8 @@
+/* Deleted everithing after version 0.01a according the local laws
+    so only version 0.01a */
 #include <stdio.h>
 int main()
 {
   printf("Hello Universe\n");
-  return 42;
+  return 41;
 }
